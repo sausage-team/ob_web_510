eed36c8fe1f60a371a5b1d35af3931a3948c9b2c
fencer-yd <982039620@qq.com>
2019-05-15 13:23:04 +0800

    去除页面权限校验

--- src/entry/index.ts
+++ src/entry/index.ts
@@ -4,12 +4,12 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class App extends Vue {
 
   public created (): void {
-    if (!this.cookies.get('user_data')) {
-      if (this.$route.name !== 'login') {
-        this.$router.push({
-          name: 'login'
-        })
-      }
-    }
+    // if (!this.cookies.get('user_data')) {
+    //   if (this.$route.name !== 'login') {
+    //     this.$router.push({
+    //       name: 'login'
+    //     })
+    //   }
+    // }
   }
 }
